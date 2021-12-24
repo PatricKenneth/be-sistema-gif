0f578a933c10ef53e49af9ca5b203251dcd8a0a0
Patric Kenneth <patrickenneth77@gmail.com>
2021-12-24 01:39:14 -0300

    setting environment

---
+++ src/main/java/br/com/besistemagif/BeSistemaGifApplication.java
@@ -0,0 +1,13 @@
+package br.com.besistemagif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSistemaGifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSistemaGifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/besistemagif/BeSistemaGifApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.besistemagif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSistemaGifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
