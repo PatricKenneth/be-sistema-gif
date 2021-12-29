e30fb55df49b484f96277fe14214b87a38932815
Patric Kenneth <patrickenneth77@gmail.com>
2021-12-29 01:11:09 -0300

    create api rest patch clients

--- src/main/java/br/com/besistemagif/domains/clients/ClientsController.java
+++ src/main/java/br/com/besistemagif/domains/clients/ClientsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("clients")
@@ -20,13 +21,37 @@ public class ClientsController {
     }
 
     @PostMapping
-    public ResponseRequest save(@RequestBody ClientsEntity newClient){
+    public ResponseRequest save(@RequestBody ClientsEntity newClient) {
         ResponseRequest response = new ResponseRequest();
         try {
             if(!ValidateAge.toValid(newClient)) {
                 throw new Exception("Data de Nascimento inválida");
             }
             ClientsEntity clients = clientsRepository.save(newClient);
+            response.setData(clients);
+            response.setError(null);
+        } catch (Exception error) {
+            response.setData(null);
+            response.setError(error.getMessage());
+        }
+        return response;
+    }
+
+    @PatchMapping
+    public ResponseRequest update(@RequestBody ClientsEntity updateClient) {
+        ResponseRequest response = new ResponseRequest();
+        try {
+            if(!ValidateAge.toValid(updateClient)) {
+                throw new Exception("Data de Nascimento inválida");
+            }
+            ClientsEntity clients = clientsRepository.findById(updateClient.getId()).get();
+            clients.setBirthDate(updateClient.getBirthDate());
+            clients.setEmail(updateClient.getEmail());
+            clients.setName(updateClient.getName());
+            clients.setGender(updateClient.getGender());
+
+            clients = clientsRepository.save(clients);
+
             response.setData(clients);
             response.setError(null);
         } catch (Exception error) {
